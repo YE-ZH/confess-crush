2fd6dcdf37f1dd7c20542944abd742a23cbdae46
YE-ZH <3259944650@qq.com>
2026-04-08 19:58:29 +0800

    Update config.js
    修改为中文版本

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    titleWeb: "表白网站",
+    introTitle: '亲爱的',
+    introDesc: `世界这么大
+    人海这么深
+    我却遇见了你
+    这一定是命运的安排`,
+    btnIntro: '^^你好^^',
+    title: '你是否从第一眼就喜欢上我了呢 🥰',
+    desc: '是否从看到我笑容的那一刻就心动了呢',
+    btnYes: '是的，我喜欢你 <33',
+    btnNo: '才没有呢 :3',
+    question: '世界上有70多亿人，为什么你会喜欢我呢 <3',
+    btnReply: '告诉我 <3',
+    reply: '喜欢你就是喜欢你，没有理由 <33333333',
+    mess: '我就知道 🥰. 我也超级喜欢你 😘😘',
+    messDesc: '今晚7点，我来接你一起去约会吧。',
+    btnAccept: '好呀好呀 <3',
+    messLink: 'https://github.com/zukahai/Confess-Crush' //这里可以修改为你的聊天链接，例如微信或QQ的链接
 }
